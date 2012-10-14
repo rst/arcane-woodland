1651e4de313dee82f2ebd9be0d886a8f206ef57e
Robert Thau <rst@ai.mit.edu>
2012-10-13 23:22:41 -0400

    Initial commit, from 'rails new' in Rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afinder::Application.config.secret_token = '27833f679074c2749ef29acf52685973d160de344b3d53fe26ac74c1338f2c1c464137af1c17351bc15811d45ee1b5c7a7afd150994701c9121a946f0fbf6e56'
