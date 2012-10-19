c8aa182e505d460c69140f632ca43135c01aac82
Robert Thau <rst@ai.mit.edu>
2012-10-19 13:41:37 -0400

    Layouts for jquery-mobile single page, and devise use of it.
    Also set layout for devise controllers in application.rb.  (Blech.)

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Afinder
   class Application < Rails::Application
+
+    config.to_prepare do
+      DeviseController.layout "devise-mobile-page"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
