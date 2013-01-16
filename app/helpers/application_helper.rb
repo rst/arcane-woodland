8f1ce6a3c84e421829a2740a5e2bba89275b844f
Robert Thau <rst@ai.mit.edu>
2013-01-15 19:38:18 -0500

    Consistent scheme for mobile_page_id and mobile_page_url.
    Just does the obvious thing --- uses request.path to initialize both,
    cleaning up punctuation for the ID.  Which should be fine so long as
    POSTs always redirect, so there's no ambiguity about whether we're
    displaying the result of show or edit (which, by Rails REST conventions,
    would be show and update on the same URL).
    
    The usual reason for POST redirection is so that a browser-level reload
    after you're done won't re-POST.  This actually applies here, but it is
    an extra HTTP request (meaning a couple of extra TCP-level round trips).
    
    jQmobile actually provides ways to fake this out; send a data-url and
    matching page ID for the redirect target.  Should consider, but this
    sets up sensible defaults, and edits are not expected to be all that
    common here...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,13 @@
 module ApplicationHelper
 
   # Helper functions to declare items of interest to mobile layouts.
-  #
-  # All this really amounts to is setting random instance variables,
-  # but it's a little more likely that typos in the names will be
-  # caught, rather than causing silent failures...
 
-  def set_mobile_page_id( arg )
-    unless @mobile_page_id.nil?
-      raise RuntimeError, "Already have mobile_page_id"
-    end
-    @mobile_page_id = arg.gsub(/[^a-zA-Z0-9_\-:.]/,'')
+  def mobile_page_id
+    @mobile_page_id ||= request.path.gsub(/[^a-zA-Z0-9_\-:.]/,'')
   end
 
-  def set_mobile_page_url( arg )
-    unless @mobile_page_url.nil?
-      raise RuntimeError, "Already have mobile_page_url"
-    end
-    @mobile_page_url = arg
+  def mobile_page_url
+    @mobile_page_url ||= request.path
   end
 
   def set_mobile_page_movable_headers_footers( arg )
@@ -25,13 +15,10 @@ module ApplicationHelper
   end
 
   def on_mobile_page_load
-    if @mobile_page_id.nil?
-      raise RuntimeError, "Have on_mobile_page_load, but no set_mobile_page_id"
-    end
     body = capture do yield end
     <<-END_STUFF.html_safe
     <script>
-      $(document).delegate("##{ @mobile_page_id }", "pageinit", #{ body })
+      $(document).delegate("##{ mobile_page_id }", "pageinit", #{ body })
     </script>
     END_STUFF
   end
